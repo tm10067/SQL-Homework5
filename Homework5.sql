04394c4001424afd9603ee1085446d234d401bed
tm10067 <110200944+tm10067@users.noreply.github.com>
2023-03-11 19:20:33 +0300

    Update Homework5.sql

--- Homework5.sql
+++ Homework5.sql
@@ -9,18 +9,18 @@ USE vk;
 
 /* Создайте представление с произвольным SELECT-запросом из прошлых уроков [CREATE VIEW] */
 
-CREATE OR REPLACE VIEW usercounter
-   AS SELECT communities.name AS community, 
-			 COUNT(user_id) AS users_count
-		FROM communities
-	    JOIN users_communities ON communities.id = users_communities.community_id
-	GROUP BY communities.id
-	ORDER BY communities.name;
+CREATE OR REPLACE VIEW usercounter AS 
+     SELECT communities.name AS community, 
+	    COUNT(user_id) AS users_count
+       FROM communities
+       JOIN users_communities ON communities.id = users_communities.community_id
+   GROUP BY communities.id
+   ORDER BY communities.name;
     
 /* Выведите данные, используя написанное представление [SELECT] */
 
 SELECT community,
-	   users_count
+       users_count
   FROM usercounter;
   
 /* Удалите представление [DROP VIEW] */
@@ -42,12 +42,12 @@ GROUP BY users.id;
 /* CTE */
 
   WITH newscounter AS (
-	   SELECT COUNT(id) AS news_count,
-		 	  user_id
+       SELECT COUNT(id) AS news_count,
+	      user_id
          FROM media
-	 GROUP BY user_id)
+     GROUP BY user_id)
 SELECT newscounter.news_count, 
-	   newscounter.user_id, 
+       newscounter.user_id, 
        users.email AS user_email
   FROM newscounter, users
  WHERE newscounter.user_id = users.id
